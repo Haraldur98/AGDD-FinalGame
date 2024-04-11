8e6c3c9d62c76d432026815f694b1f5411860aad
raggiolafs00 <ragnaro21@ru.is>
2024-04-11 16:25:48 +0000

    timer slider cash2

--- Assets/Scripts/MiniGameManager.cs
+++ Assets/Scripts/MiniGameManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MiniGameManager : MonoBehaviour
 {
@@ -16,16 +18,16 @@ public class MiniGameManager : MonoBehaviour
     public TextMeshProUGUI cashText;
     public int score;
     public int decrement;
-    private float timeToDisplay = 0;
+    private float timeToDisplay;
     private float scoreDecrementTimer = 0f;
     public Slider cashSlider;
-
     public UnityEvent onMiniGameEnd;
     public Camera miniGameCamera;
 
     public Vector3 mainCameraPos;
     int totalPipes = 0;
     float[] rotations = { 0, 90, 180, 270 };
+    public int difficulty;
     StartMiniGame startMiniGame;
 
     IEnumerator Start()
@@ -34,10 +36,10 @@ public class MiniGameManager : MonoBehaviour
         totalPipes = pipeHolder.transform.childCount;
         endPipe = endPipeGameObject.GetComponent<MiniGameUnoPipe>();
         pipes = new GameObject[totalPipes];
-        cashSlider.maxValue = score;
-
         startMiniGame = GameObject.FindObjectOfType<StartMiniGame>();
-
+        difficulty = startMiniGame.difficulty;
+        adjustScore();
+        cashSlider.maxValue = score / decrement;
         // Initialize pipes with random rotations
         for (int i = 0; i < pipes.Length; i++)
         {
@@ -88,8 +90,18 @@ public class MiniGameManager : MonoBehaviour
         }
 
         if (timerIsRunning)
-        {
-            timeToDisplay += Time.deltaTime; // Ensure the last second is counted
+        {   
+            // Ensure the last second is counted
+            // decrement score every second:
+            scoreDecrementTimer += Time.deltaTime;
+            timeToDisplay += Time.deltaTime;
+            cashSlider.value = timeToDisplay;
+            if (scoreDecrementTimer >= 1)
+            {
+                score -= decrement;
+                cashText.text = "<color=green>$</color>:" + score;
+            }
+            
 
         }
     }
